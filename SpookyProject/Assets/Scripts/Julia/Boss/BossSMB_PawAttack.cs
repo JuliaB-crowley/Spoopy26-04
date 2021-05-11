8e892718ad0f002f94707e031d3ad543c2efc84e
Ju-Kraken <j.bourdin@rubika-edu.com>
2021-05-11 20:13:24 +0200

    Boss presque fini - à tester

--- SpookyProject/Assets/Scripts/Julia/Boss/BossSMB_PawAttack.cs
+++ SpookyProject/Assets/Scripts/Julia/Boss/BossSMB_PawAttack.cs
@@ -4,16 +4,61 @@ using UnityEngine;
 
 public class BossSMB_PawAttack : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
+    public JUB_BossBehavior boss;
+    float timeSinceBuildup, timeSinceRecovery, timeSinceHitspan;
+    bool isInBuildup, isInRecovery, isInHitspan;
 
-    //}
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timeSinceHitspan = timeSinceBuildup = timeSinceRecovery = 0;
+        isInBuildup = true;
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (isInBuildup)
+        {
+            timeSinceBuildup += Time.deltaTime;
+            if (timeSinceBuildup >= boss.buildupTime)
+            {
+                isInHitspan = true;
+                isInBuildup = false;
+                timeSinceBuildup = timeSinceHitspan = 0;
+            }
+        }
 
+        if (isInHitspan)
+        {
+            timeSinceHitspan += Time.deltaTime;
+            HitSpan();
+            if (timeSinceHitspan >= boss.hitspanTime)
+            {
+                isInHitspan = false;
+                isInRecovery = true;
+                timeSinceHitspan = timeSinceRecovery = 0;
+            }
+        }
+
+        if (isInRecovery)
+        {
+            timeSinceRecovery += Time.deltaTime;
+            if (timeSinceRecovery >= boss.recoveryTime)
+            {
+                isInRecovery = false;
+                timeSinceRecovery = 0;
+                animator.Play("Idle");
+            }
+        }
+
+        void HitSpan()
+        {
+            if(boss.pawCollider.IsTouchingLayers(boss.playerLayer))
+            {
+                boss.maeve.TakeDamages(boss.pawDamages);
+            }
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
