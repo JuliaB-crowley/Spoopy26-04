4f12b770b7dcd39b4731fa7fa0e5f7edd6050873
Ju-Kraken <j.bourdin@rubika-edu.com>
2021-05-22 17:35:45 +0200

    Mise en place du tuto, réparation caisses

--- SpookyProject/Assets/Scripts/Julia/Dialogues/JUB_SpeakingEvent.cs
+++ SpookyProject/Assets/Scripts/Julia/Dialogues/JUB_SpeakingEvent.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class JUB_SpeakingEvent : MonoBehaviour
 {
     public JUB_Dialogue dialogue;
+    bool wasSaidOnce = false;
 
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<JUB_DialogueManager>().StartDialogue(dialogue);
+        if (!wasSaidOnce)
+        {
+            GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<JUB_DialogueManager>().StartDialogue(dialogue);
+            wasSaidOnce = true;
+        }
     }
 
 }
